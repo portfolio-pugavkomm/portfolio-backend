2854c2db38f4bf2d8d54a1413f9167e42f380faf
Pugavkomm <slavapugavko2@gmail.com>
2025-03-26 22:38:11 -0300

    Move secret value to config file

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-c&)rovz$b=pxr1nh&n)j$_w6zqm(+g0@pog9rcx$qx(nyrxy3!"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
