5e9bd81675ffbc2befdb92f5d224ed0a12f58720
Pugavkomm <slavapugavko2@gmail.com>
2025-03-26 22:42:17 -0300

    unfold admin theme

--- core/settings.py
+++ core/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS: list[str] = []
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",  # theme
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
